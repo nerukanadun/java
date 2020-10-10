600ebedc1f4fd4eda0ae2408d04172f45a0b4920
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-10-10 23:42:21 +0530

    Update increaseval.java

--- increaseval.java
+++ increaseval.java
@@ -1,1 +1,12 @@
+int nu01 = 10000;
+int level = 0;
+int increase = 35000;
 
+for (int i = 0; i < 100; i++) {
+    nu01 = nu01 + increase;
+    level++;
+    System.out.println(nu01);
+
+    if (level > 50) {
+        increase = 45000;
+    }
